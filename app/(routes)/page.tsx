09838eb97b23f3ec2616ab241cf2a40684d5499e
Davide Filosa <davideclaypool@gmail.com!>
2023-09-20 07:31:44 +0200

    created home page and featured products list

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,22 @@
 import getBillboards from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import React from "react";
 import Billboard from "../components/billboard";
+import ProductList from "../components/productList";
 import Container from "../components/ui/Container";
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboards = await getBillboards(
-    "9ea603df-2e1a-429d-bb44-e486913b7bc1"
+    "6b150501-d809-4bb2-b945-7afdcdd2567b"
   );
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboards} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
