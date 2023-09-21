a830589e7c43c70502093f257b6896922c24a2c3
Davide Filosa <davideclaypool@gmail.com!>
2023-09-21 10:15:57 +0200

    added Add to Cart button

--- app/components/info.tsx
+++ app/components/info.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { ShoppingCart } from "lucide-react";
 import { Product } from "@/types";
+import Button from "./ui/button";
 import Currency from "./ui/currency";
 
 interface InfoProps {
@@ -14,16 +18,24 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </p>
       </div>
       <hr className="my-4" />
-      <div className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Size:</h3>
-        <div>{data?.size?.name}</div>
-      </div>
-      <div className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Color:</h3>
-        <div
-          className="h-6 w-6 rounded-full border border-gray-600"
-          style={{ backgroundColor: data?.color?.value }}
-        />
+      <div className="flex flex-col gap-y-6">
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Size:</h3>
+          <div>{data?.size?.name}</div>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Color:</h3>
+          <div
+            className="h-6 w-6 rounded-full border border-gray-600"
+            style={{ backgroundColor: data?.color?.value }}
+          />
+        </div>
+        <div className="mt-10 flex items-center gap-x-3">
+          <Button className="flex items-center gap-x-2">
+            Add to Cart
+            <ShoppingCart />
+          </Button>
+        </div>
       </div>
     </div>
   );
