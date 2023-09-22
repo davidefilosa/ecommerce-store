01a4fa51d917eb0b036899a8c0fc946962ec4c56
Davide Filosa <davideclaypool@gmail.com!>
2023-09-22 08:08:29 +0200

    create filter functionality for mobile version

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -10,6 +10,7 @@ import Container from "@/app/components/ui/Container";
 import NoResults from "@/app/components/ui/no-results";
 import ProductCard from "@/app/components/ui/product-card";
 import Filter from "./components/filter";
+import MobileFilters from "./components/mobile-filters";
 
 interface CategoryPageProps {
   params: {
@@ -40,6 +41,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
       <Billboard data={category.billboard} />
       <div className="px-4 sm:px-6 lg:px-8 pb-24">
         <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+          <MobileFilters sizes={sizes} colors={colors} />
           <div className="hidden lg:block">
             <Filter valueKey="sizeId" name="Sizes" data={sizes} />
             <Filter valueKey="colorId" name="Colors" data={colors} />
